ba5e5f6bff2707acad71f80965c2649500c03bf9
piraha-tribe <piraha.tribe@gmail.com>
2025-08-27 11:37:24 +0800

    v6.1

--- source/admin/widgets/csv-gui.js
+++ source/admin/widgets/csv-gui.js
@@ -1,6 +1,7 @@
 const { useState, useEffect, useRef } = React;
+const h = React.createElement;
 
-// Control Component (後台編輯)
+// Control Component (後台可互動編輯)
 function CSVControl({ value = [], onChange, forID, classNameWrapper }) {
   const tableRef = useRef(null);
   const [data, setData] = useState(value);
@@ -12,7 +13,13 @@ function CSVControl({ value = [], onChange, forID, classNameWrapper }) {
       data: data,
       reactiveData: true,
       layout: "fitDataStretch",
-      columns: data[0] ? Object.keys(data[0]).map(k => ({ title: k, field: k, editor: "input" })) : [],
+      columns: data[0]
+        ? Object.keys(data[0]).map((k) => ({
+            title: k,
+            field: k,
+            editor: "input",
+          }))
+        : [],
       cellEdited: () => onChange(data),
     });
 
@@ -21,36 +28,51 @@ function CSVControl({ value = [], onChange, forID, classNameWrapper }) {
 
   const addRow = () => {
     const newRow = {};
-    if (data[0]) Object.keys(data[0]).forEach(k => newRow[k] = "");
+    if (data[0]) Object.keys(data[0]).forEach((k) => (newRow[k] = ""));
     setData([...data, newRow]);
   };
 
   const addColumn = () => {
     const colName = prompt("輸入欄位名稱");
     if (!colName) return;
-    const newData = data.map(r => ({ ...r, [colName]: "" }));
+    const newData = data.map((r) => ({ ...r, [colName]: "" }));
     setData(newData);
   };
 
-  return (
-    <div className={classNameWrapper} id={forID}>
-      <div ref={tableRef}></div>
-      <button type="button" onClick={addRow}>新增列</button>
-      <button type="button" onClick={addColumn}>新增欄</button>
-    </div>
+  return h(
+    "div",
+    { className: classNameWrapper, id: forID },
+    h("div", { ref: tableRef }),
+    h(
+      "button",
+      { type: "button", onClick: addRow, style: { marginRight: "5px" } },
+      "新增列"
+    ),
+    h(
+      "button",
+      { type: "button", onClick: addColumn },
+      "新增欄"
+    )
   );
 }
 
 // Preview Component (前台只讀)
 function CSVPreview({ value = [] }) {
-  if (!value.length) return <div>無資料</div>;
+  const previewRef = useRef(null);
 
-  const columns = Object.keys(value[0]).map(k => ({ title: k, field: k }));
-  
-  return <div id="csv-preview" ref={el => {
-    if (!el) return;
-    new Tabulator(el, { data: value, columns, layout: "fitDataStretch", reactiveData: false });
-  }}></div>;
+  useEffect(() => {
+    if (!previewRef.current) return;
+    if (!value.length) return;
+
+    new Tabulator(previewRef.current, {
+      data: value,
+      reactiveData: false,
+      layout: "fitDataStretch",
+      columns: Object.keys(value[0]).map((k) => ({ title: k, field: k })),
+    });
+  }, [previewRef, value]);
+
+  return h("div", { ref: previewRef });
 }
 
 // 註冊 Widget
